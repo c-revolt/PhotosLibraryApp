b828e663c8f827e19b75e00b48d0dff4aa821aad
Alexander <a.pride.xxi@tutanota.com>
2023-04-12 11:38:30 +0300

    Setup

--- PhotosLibraryApp/Modules/HomeModule/HomeViewController.swift
+++ PhotosLibraryApp/Modules/HomeModule/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  PhotosLibraryApp
 //
 //  Created by Александр Прайд on 12.04.2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
