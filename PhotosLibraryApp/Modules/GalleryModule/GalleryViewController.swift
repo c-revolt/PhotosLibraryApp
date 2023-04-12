3d27e1eada19d773387ac5f45467fdfc3c8f38a7
Alexander <a.pride.xxi@tutanota.com>
2023-04-12 16:02:46 +0300

    Complete GalleryViewController

--- PhotosLibraryApp/Modules/GalleryModule/GalleryViewController.swift
+++ PhotosLibraryApp/Modules/GalleryModule/GalleryViewController.swift
@@ -9,13 +9,104 @@ import UIKit
 
 class GalleryViewController: UIViewController {
 
+    // properties
+    private var collectionView: UICollectionView?
+    
+    private lazy var addBarButtonItem: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: .add,
+                               target: self,
+                               action: #selector(addBarButtonTapped))
+    }()
+    
+    private lazy var sharedBarButtonItem: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: .action,
+                               target: self,
+                               action: #selector(sharedBarButtonTapped))
+    }()
+    
+    // lifecicle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .red
-        
         navigationItem.title = "Gallery"
     
+        setupNavigationBar()
+        createCollectionView()
+        setupSearchBar()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        collectionView?.frame = view.bounds
+    }
+    
+    // MARK: - Setup UI Elements
+    private func createCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        guard let collectionView = collectionView else { return }
+        
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellID")
+    }
+    
+    private func setupNavigationBar() {
+        let titleLabel = UILabel()
+        titleLabel.text = "PHOTOS"
+        titleLabel.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        titleLabel.textColor = .lightText
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
+        navigationItem.rightBarButtonItems = [sharedBarButtonItem, addBarButtonItem]
+    }
+    
+    private func setupSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self 
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension GalleryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    
+}
+
+// MARK: - UICollectionViewDelegate
+extension GalleryViewController: UICollectionViewDelegate {
+    
+}
+
+// MARK: - UISearchBarDelegate
+extension GalleryViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
+}
+
+// MARK: - NavigationItems Actions
+extension GalleryViewController {
+    @objc private func addBarButtonTapped() {
+        print(#function)
+    }
+    
+    @objc private func sharedBarButtonTapped() {
+        print(#function)
     }
 }
 
